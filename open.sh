19c70357393d10bef21c3618fec0dda10b88a0f1
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-02-25 15:34:09 +0700

    z

--- open.sh
+++ open.sh
@@ -7,7 +7,7 @@
                      projectname="template" #ตั้งชื่อ projhttpsect ให้เหมือนกัน
                      google_sheet="https://docs.google.com/spreadsheets/d/1H7UgGtAy3JKvulLqGXVm5zeNq1FdNKonMxxBETQtjXQ/edit?gid=1610289956#gid=1610289956" 
                      id="1"                 #เปลี่ยน id ทุกครั้งที่ยิง
-                     user="1";            #จำนวนผู้ใช้งาน
+                     user="2";            #จำนวนผู้ใช้งาน
                      durationx="1";        #วินาที
                      scenario="1"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
                      cid="1"                #scenario="2" ยิงเเบบกำหนด VUs  (กำหนดว่า user x คน ใช้ระบบ x วินาที)
@@ -61,7 +61,7 @@ if [ ! -d "$report_path" ]; then
   # ถ้าไม่มีให้สร้างโฟลเดอร์ folder
   mkdir -p "$report_path"
 fi
-if [ "$status" = "report" ]; then
+if [ "$status" = "normal" ]; then
   if [ -d "$report_path" ] && [ "$(ls -A $report_path)" ]; then
     echo "❌ Folder dupicate/Change ID"
     exit 1
